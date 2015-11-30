5a7a53f0a7d287b05618c9df756e4bccfdbbe6bb
wei.Deng <dengwei201020@163.com>
2015-11-30 15:07:47 +0800

    vector类添加成员函数及逻辑操作符

--- vector/Vector.h
+++ vector/Vector.h
@@ -9,6 +9,8 @@
 #include"Algorithm.h"
 #include"__Type_traits.h"
 #include"Iterator.h"
+#include<utility>
+#include<new>
 
 
 namespace MyCppSTL{
@@ -214,6 +216,107 @@ namespace MyCppSTL{
 			}
 		}
 
+		void pop_back()
+		{
+			if (empty())
+			{
+				throw std::runtime_error("empty vector");
+			}
+			destroy(_last - 1);
+			_last -= 1;
+		}
+
+		iterator erase(iterator pos)   //移除指定位置的元素
+		{
+			if (pos > end() || pos < begin())
+			{
+				throw std::out_of_range("out of range");
+			}
+			for (auto it = pos; it < end()-1; ++it)
+			{
+				*it = *(it + 1);
+			}
+			destroy(_last - 1);
+			_last -= 1;
+			return pos;
+		}
+
+		iterator erase(iterator first, iterator last) //移除指定区间
+		{
+			difference_type n = last - first;
+			auto pos = first;
+			for (; last < end(); )
+			{
+				*first++ = *last++;
+			}
+			destroy(first, end());
+			_last -= (size_type)n;
+			return pos;
+		}
+
+		void clear()
+		{
+			erase(begin(), end());
+		}
+
+		void resize(size_type count, const T &value)
+		{
+			if (count < size())
+			{
+				erase(begin() + count, end());
+			}
+			else
+			{
+				insert(end(), count-size(), value);
+			}
+		}
+
+		void resize(size_type count)
+		{
+			resize(count, T());
+		}
+
+		template< class... Args >
+		iterator emplace(const_iterator pos, Args&&... args) //pos之前直接构造元素
+		{
+			auto pos_ptr = _first + (pos - begin());
+			if ((size() + 1) <= capacity())//空间足够
+			{
+				
+				for (auto it =_last - 1; it >= pos_ptr; --it)
+				{
+					construct(it + 1, *it);
+				}
+				construct(pos_ptr, std::forward<Args>(args)...);
+				++_last;
+				return iterator(pos_ptr);
+			}
+			else
+			{
+				const size_type new_size = ((size() == 0) ? 1 : 2 * size());  //按照两倍的关系分配
+				auto count = pos_ptr - _first;
+				reserve(new_size);
+				pos_ptr = _first + count;
+				for (auto it = _last - 1; it >= pos_ptr; --it)
+				{
+					construct(it + 1, *it);
+				}
+				construct(pos_ptr, std::forward<Args>(args)...);
+				++_last;
+				return iterator(pos_ptr);
+			}
+
+
+
+		}
+
+		template< class... Args >
+		void emplace_back(Args&&... args)  //在末尾构造元素
+		{
+			emplace(end(), args...);
+		}
+
+
 		iterator insert(iterator pos, const T&val)
 		{
 			/*首先判断增加了这个元素后会不会超出capacity*/
@@ -244,44 +347,21 @@ namespace MyCppSTL{
 			}
 		}
 
-		/*
-		void insert(iterator pos, size_type count, const T& value)//插入count个value
-		{
-			if (pos > end())
-			{
-				throw std::out_of_range("out of range");
-			}
-			if ((size() + count) <= capacity())
-			{
-				uninitialized_fill_n(_last, count, *(_last - 1));//先构造出count个空间
-				for (auto it = pos; it < _last; ++it) //移动元素
-				{
-					*(it + count) = *it;
-				}
-		//  //这样的设计是不是很不好？
-				for (size_type n=0; n<count;++n)//插入元素
-				{
-					*pos++ = value;
-				}
-				_last += count;
-				
-			}
-			else  //空间不够
-			{
-				difference_type distance = pos - begin();
-				const size_type new_size = ((size() == 0) ? 1 : (2 * (size()+count)));  //按照两倍的关系分配
-				reserve(new_size);
-				auto new_pos = _first + distance;//恢复位置
-				insert(new_pos, count,value);  //递归调用
-
-			}
-		}
-		*/
 		template< class InputIt >
 		void insert(iterator pos, InputIt first, InputIt last)//接受迭代器指定的范围,要与接受两个整数的区别
 		{
 			insert_aux(pos, first, last, std::is_integral<InputIt>::type());
 		}
+
+		
+		template<class T>
+		void insert(iterator pos, size_type count, const T& value)//接受迭代器指定的范围,要与接受两个整数的区别
+		{
+			insert_aux(pos, count, value, std::is_integral<T>::type());
+		}
+		
+
+		//
 
 
 	private:
@@ -320,7 +400,7 @@ namespace MyCppSTL{
 
 	   //辅助函数，这是整数
 	   template<class InputIt>
-	   void insert_aux(iterator pos, InputIt count, InputIt value,std::true_type)//插入count个value
+	   void insert_aux(iterator pos, InputIt count, const value_type&value,std::true_type)//插入count个value
 	   {
 		   if (pos > end())
 		   {
@@ -328,10 +408,26 @@ namespace MyCppSTL{
 		   }
 		   if ((size() + count) <= capacity())
 		   {
+			   difference_type distance = end() - pos;
 			   uninitialized_fill_n(_last, count, *(_last - 1));//先构造出count个空间
-			   for (auto it = pos; it < _last; ++it) //移动元素
+			   if (((size_type)distance)<=((size_type)count))  //插入元素的数量count大于pos到_last元素的数量
 			   {
-				   *(it + count) = *it;
+				   for (auto it = pos; it < _last; ++it) //移动元素
+				   {
+					   *(it + count) = *it;
+				   }
+			   }
+			   else //数量小于distance，那么先移动尾部的元素，在移动pos之后的元素
+			   {
+				   for (auto it2 = end()-1; it2>pos-1; --it2)
+				   {
+					   *(it2 + count) = *it2;
+				   }
+			       for (auto it = pos; it < pos+count; ++it) //移动元素
+				   {
+						   *(it + count) = *it;
+				    }
+				
 			   }
 			   //  //这样的设计是不是很不好？
 			   for (size_type n = 0; n<((size_type)count); ++n)//插入元素
@@ -359,13 +455,29 @@ namespace MyCppSTL{
 		   {
 			   throw std::out_of_range("out of range");
 		   }
+		   difference_type distance = end() - pos;
 		   difference_type count = last - first;//要插入元素的数量
 		   if (((size_type)count + size()) <= capacity())//有空间，插入
 		   {
 			   uninitialized_fill_n(_last, count, *(_last - 1));//先构造出count个空间
-			   for (auto it = pos; it < _last; ++it) //移动元素
+			   if (((size_type)distance) <= ((size_type)count))  //插入元素的数量count大于pos到_last元素的数量
 			   {
-				   *(it + count) = *it;
+				   for (auto it = pos; it < _last; ++it) //移动元素
+				   {
+					   *(it + count) = *it;
+				   }
+			   }
+			   else //数量小于distance，那么先移动尾部的元素，在移动pos之后的元素
+			   {
+				   for (auto it2 = end() - 1; it2>pos - 1; --it2)
+				   {
+					   *(it2 + count) = *it2;
+				   }
+				   for (auto it = pos; it < pos + count; ++it) //移动元素
+				   {
+					   *(it + count) = *it;
+				   }
+
 			   }
 			   for (size_type n = 0; n<((size_type)count); ++n)//插入元素
 			   {
@@ -401,14 +513,51 @@ namespace MyCppSTL{
 			   throw std::out_of_range("out of range");
 		   }
 	   }
-	   
-	   
+
 	
 	};
 
+	//非成员函数，比较操作
+	template<class T, class alloc = MyCppSTL::allocator<T>>
+	bool operator==(const vector<T>&lhs, const vector<T>&rhs)
+	{
+		if (lhs.size() == rhs.size())
+		{
+			return (equal(lhs.begin(), lhs.end(), rhs.begin()));
+		}
 
+		return false;
+	}
 
+	template<class T, class alloc = MyCppSTL::allocator<T>>
+	bool operator!=(const vector<T>&lhs, const vector<T>&rhs)
+	{
+		return !(lhs == rhs);
+	}
 
+	template<class T, class alloc = MyCppSTL::allocator<T>>
+	bool operator<(const vector<T>&lhs, const vector<T>&rhs)
+	{
+		return(MyCppSTL::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()));
+	}
+
+	template<class T, class alloc = MyCppSTL::allocator<T>>
+	bool operator>(const vector<T>&lhs, const vector<T>&rhs)
+	{
+		return (rhs < lhs);
+	}
+
+	template<class T, class alloc = MyCppSTL::allocator<T>>
+	bool operator<=(const vector<T>&lhs, const vector<T>&rhs)
+	{
+		return !(lhs>rhs);
+	}
+
+	template<class T, class alloc = MyCppSTL::allocator<T>>
+	bool operator>=(const vector<T>&lhs, const vector<T>&rhs)
+	{
+		return !(lhs < rhs);
+	}
 
 }
 
