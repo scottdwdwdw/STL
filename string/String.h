58cf93446b70d651baaf6ac0af9d48d18f047019
wei.Deng <dengwei201020@163.com>
2016-01-05 20:28:18 +0800

    添加构造函数，完成迭代器设计

--- string/String.h
+++ string/String.h
@@ -3,20 +3,22 @@
 
 #include"Allocator.h"
 #include"Iterator.h"
+#include"Algorithm.h"
 
 
 namespace MyCppSTL
 {
-	template<class CharT=char>
-	class string_const_iterator:public iterator<MyCppSTL::random_access_iterator_tag,CharT>
+
+
+	class string_const_iterator:public iterator<MyCppSTL::random_access_iterator_tag,char>
 	{
 	public: //内嵌型别定义
-		typedef const CharT value_type;
-		typedef const CharT& reference;
-		typedef const CharT* pointer;
+		typedef const char value_type;
+		typedef const char& reference;
+		typedef const char* pointer;
 		typedef std::size_t size_type;
 		typedef std::ptrdiff_t difference_type;
-		typedef string_const_iterator<CharT> _MyIter;
+		typedef string_const_iterator _MyIter;
 	private: //数据
 		pointer _p; //指向当前位置
 	public:
@@ -49,15 +51,239 @@ namespace MyCppSTL
 			++_p;
 			return *this;
 		}
+		_MyIter operator++(int) //后置递增
+		{
+			auto tmp = *this;
+			++*this;
+			return tmp;
+		}
+		_MyIter&operator--() //前置递减
+		{
+			--_p;
+			return *this;
+		}
+		_MyIter operator--(int)
+		{
+			auto tmp = *this;
+			--*this;
+			return tmp;
+		}
+
+		_MyIter&operator+=(size_type n)
+		{
+			_p += n;
+			return *this;
+		}
+		_MyIter operator+(size_type n)
+		{
+			auto tmp = *this;
+			tmp += n;
+			return tmp;
+		}
+		_MyIter&operator-=(size_type n)
+		{
+			_p -= n;
+			return *this;
+		}
+		_MyIter operator-(size_type n)
+		{
+			auto tmp = *this;
+			tmp -= n;
+			return tmp;
+		}
+
+		difference_type operator-(_MyIter&other)
+		{
+			return (_p - other._p);
+		}
+
+		//逻辑操作
+		bool operator==(_MyIter& other)
+		{
+			return (_p == other._p);
+		}
+		bool operator!=(_MyIter& other)
+		{
+			return !(*this == other);
+		}
+	};
+
+
+	class string_iterator:public string_const_iterator
+	{
+	public:
+		typedef char value_type;
+		typedef char& reference;
+		typedef char* pointer;
+		typedef std::size_t size_type;
+		typedef std::ptrdiff_t difference_type;
+
+		typedef string_const_iterator _MyBase;
+		typedef string_iterator _MyIter;
+
+		//construct
+		string_iterator() :string_const_iterator() {}
+		string_iterator(pointer p) :string_const_iterator(p) {}
+		string_iterator(string_const_iterator&it) :string_const_iterator(it) {}
+
+		//
+		reference operator*()
+		{
+			return (reference)**(_MyBase*)this;
+         }
+		reference operator[](size_type n)
+		{
+			return (reference)(*(_MyBase*)this).operator[](n);
+			//reference (*(_MyBase*)this)[n];
+		}
+		
+		//前置递增
+		_MyIter&operator++()
+		{
+			++*(_MyBase*)this;
+			return *this;
+		}
+		//后置递增
+		_MyIter operator++(int)
+		{
+			auto tmp = *this;
+			++*this;
+			return tmp;
+		}
+		//前置递减
+		_MyIter operator--()
+		{
+			--*(_MyBase*)this;
+			return *this;
+		}
+		//后置递减
+		_MyIter operator--(int)
+		{
+			auto tmp = *this;
+			--*this;
+			return tmp;
+		}
+
+		//复合加
+		_MyIter&operator+=(size_type n)
+		{
+			*(_MyBase*)this += n;
+			return *this;
+		}
+		//加
+		_MyIter operator+(size_type n)
+		{
+			auto tmp = *this;
+			tmp += n;
+			return tmp;
+		}
+		//复合减
+		_MyIter&operator-=(size_type n)
+		{
+			*(_MyBase*)this -= n;
+			return *this;
+		}
+		//减
+		
+		_MyIter operator-(size_type n)
+		{
+			auto tmp = *this;
+			tmp -= n;
+			return tmp;
+		}
+		
+		//迭代器的距离
+		difference_type operator-(_MyIter&other)
+		{
+			return (*(_MyBase*)this - (*(_MyBase*)&other));
+		}
+		//逻辑操作
 
 	};
 
 
-	template<class CharT=char,class Alloc=MyCppSTL::allocator<CharT>>
 	class string
 	{
+	public:
+		typedef char         value_type;
+		typedef char*        pointer;
+		typedef const char*  const_pointer;
+		typedef  char&       reference;
+		typedef  const char& const_reference;
+		typedef std::size_t   size_type;
+		typedef std::ptrdiff_t difference_type;
+
+		typedef MyCppSTL::allocator<char>  alloc;     //空间分配器
+		typedef string_const_iterator    const_iterator;
+		typedef string_iterator          iterator;
+
 		
+	private: //member
+		pointer _start;     //指向字符串的开始
+		pointer _finish;    //指向字符串末尾
+		pointer _end_of_storage; //指向存储空间末尾.
+		enum { _default_size = 8 };
+	public:
+		static const size_type npos;
+		//construct
+		explicit string():_start(0),_finish(0),_end_of_storage(0) //default construct,empty string
+		{
+			allocate_block(_default_size);
+		}
+		string(size_type count, char value) //count value
+		{
+			allocate_block(count+1);   //分配空间
+			_finish = _start + count;
+			uninitialized_fill(_start, _finish, value);  //
+			*_finish = char();
+		}
+		string(const string&other, size_type pos, size_type count = string::npos)
+		{
+			if (pos > size())
+			{
+				std::out_of_range("out of range");
+			}
+			range_initializer(other._start + pos, other._start + pos+min(count,size()-pos));
+		}
+
+		string(const char*s, size_type count)
+		{
+			range_initializer(s, s + count);
+		}
+
+		//迭代器
+		iterator begin() { return iterator(_start); }
+		const_iterator begin()const { return const_iterator(_start); }
+		const_iterator cbegin()const { return const_iterator(_start); }
+		iterator end() { return iterator(_finish); }
+		const_iterator end() const { return const_iterator(_finish); }
+		const_iterator cend() const { return const_iterator(_finish); }
+
+		size_type size() const { return _finish - _start; }
+		//辅助函数
+	private:
+		void allocate_block(size_type n) //分配空间
+		{
+			_start = alloc::allocate(n);
+			_finish = _start;
+			_end_of_storage = _start + n;
+		}
+		void deallocate_block()        //释放空间
+		{
+			alloc::deallocate(_start, _end_of_storage - _start);
+			
+		}
+		template<class Iterator>
+		void range_initializer(Iterator first, Iterator last)
+		{
+			auto n = last - first;
+			allocate_block(n+1); //分配空间
+			MyCppSTL::uninitialized_copy(first, last, _start);
+			*_finish = char();   //添加结束符
+		}
 	};  // end-string
+
+	const string::size_type string::npos =(string::size_type)-1;
 
 
 
